dffcdc59d6f471460155e527858e2aff520f6877
jorge <jorgemartinezpizarro@gmail.com>
2025-01-02 00:19:05 +0100

    rework start page with some random links

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,13 @@
 const WeatherAndReports = () => {
 
 	return (
-    <div style={{fontSize: "30px", background: "#222", margin: "100px auto", width: "800px", height: "400px", verticalAlign: "middle"}}>
-      <ul style={{listStyleType: "none"}}>
-        <li style={{padding: "8px"}}>
-          <a style={{textDecoration: "none", color: "white"}} href={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/reports/report_cloud.html"}>
-            <img style={{margin: "0 4px"}} height="26" width="26" src={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/core/preview?fileId=47020&x=1920&y=1080&a=true&etag=c9bab916602836e6a5fe852e66890024"}></img>
-            Nginx report for cloud.ideniox.com
-          </a>
-        </li>
-        <li style={{padding: "8px"}}>
-          <a style={{textDecoration: "none", color: "white"}} href={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/reports/report_bitcoin.html"}>
-            <img style={{margin: "0 4px"}} height="26" width="26" src={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/core/preview?fileId=47020&x=1920&y=1080&a=true&etag=c9bab916602836e6a5fe852e66890024"}></img>
-            Nginx report for bitcoinprivacy.net
-          </a>
-        </li>
-        <li style={{padding: "8px"}}>
-          <a style={{textDecoration: "none", color: "white"}} href={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/reports/report_ideniox.html"}>
-            <img style={{margin: "0 4px"}} height="26" width="26" src={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/core/preview?fileId=31375&x=1920&y=1080&a=true&etag=1c029b213cdca0de1a5f0f602a8721e5"}></img>
-            Nginx report for ideniox.com
-          </a>
-        </li>
-        <li style={{padding: "8px"}}>
-          <a style={{textDecoration: "none", color: "white"}} href={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/reports/report_math.html"}>
-            <img style={{margin: "0 4px"}} height="26" width="26" src={process.env.NEXT_PUBLIC_NEXTCLOUD_URL+"/core/preview?fileId=47020&x=1920&y=1080&a=true&etag=c9bab916602836e6a5fe852e66890024"}></img>
-            Nginx report for math.ideniox.com
-          </a>
-        </li>
+    <div style={{fontSize: "27px", background: "#222", margin: "100px auto", width: "800px", height: "400px", verticalAlign: "middle", alignItems:"center"}}>
+      <ul style={{listStyleType: "none", }}>
+        <li><a href="/bookmarks/pages/code">Code & backups</a></li>
+        <li><a href="/bookmarks/pages/games">Gaming platform</a></li>
+        <li><a href="/bookmarks/pages/monitor">System overview</a></li>
+        <li><a href="/bookmarks/pages/paythering">Lightning network management</a></li>
+        <li><a href="/bookmarks/pages/trainer">Personal GPT trainer</a></li>
       </ul>
     </div>
   );
---
+++ app/pages/code/page.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { List, ListItem, ListItemText, ListItemIcon, Typography, Divider, Box, Link } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import BackupIcon from '@mui/icons-material/Backup';
+import CloudIcon from '@mui/icons-material/Cloud';
+import BitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import MathIcon from '@mui/icons-material/Functions';
+
+const resources = [
+  { icon: <GitHubIcon />, text: 'BRAIN Sources', link: 'https://github.com/jorgemartinezpizarro/brain' },
+  { icon: <GitHubIcon />, text: 'Custom UI Sources', link: 'https://github.com/jorgemartinezpizarro/bookmarks' },
+  { icon: <BackupIcon />, text: 'Backups Docker Registry', link: 'https://hub.docker.com/repository/docker/jorgemartinezpizarro/brain/tags' },
+];
+
+const visitors = [
+  { icon: <CloudIcon />, text: 'Visitors for cloud.ideniox.com', link: 'https://cloud.ideniox.com/reports/report_cloud.html' },
+  { icon: <BitcoinIcon />, text: 'Visitors for bitcoinprivacy.net', link: 'https://cloud.ideniox.com/reports/report_bitcoin.html' },
+  { icon: <CloudIcon />, text: 'Visitors for ideniox.com', link: 'https://cloud.ideniox.com/reports/report_ideniox.html' },
+  { icon: <MathIcon />, text: 'Visitors for math.ideniox.com', link: 'https://cloud.ideniox.com/reports/report_math.html' },
+  { icon: <MathIcon />, text: 'Visitors for nube.ideniox.com', link: 'https://cloud.ideniox.com/reports/report_nube.html' },
+  { icon: <MathIcon />, text: 'Visitors for home.ideniox.com', link: 'https://cloud.ideniox.com/reports/report_home.html' },
+];
+
+const ResourcePage = () => {
+  return (
+    <Box sx={{ color: "white", maxWidth: 600, margin: 'auto', padding: 4 }}>
+      {/* Encabezado */}
+      <Typography variant="h4" align="center" gutterBottom>
+        Project Resources
+      </Typography>
+      <Typography variant="h6" align="center" gutterBottom>
+        TODO: make it customizable
+      </Typography>
+
+      {/* Lista de Recursos */}
+      <Typography variant="h6" gutterBottom>
+        Resources
+      </Typography>
+      <List>
+        {resources.map((item, index) => (
+          <ListItem key={index} component="a" href={item.link} target="_blank" rel="noopener noreferrer" sx={{ textDecoration: 'none' }}>
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText style={{color: "white"}} primary={item.text} />
+          </ListItem>
+        ))}
+      </List>
+
+      <Divider sx={{ marginY: 2 }} />
+
+      {/* Lista de Visitantes */}
+      <Typography variant="h6" gutterBottom>
+        Visitors
+      </Typography>
+      <List>
+        {visitors.map((item, index) => (
+          <ListItem key={index} component="a" href={item.link} target="_blank" rel="noopener noreferrer" sx={{ textDecoration: 'none' }}>
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText style={{color: "white"}}  primary={item.text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default ResourcePage;
