c1eba1fed51da28e3f3ca921bb042e3a48bf28cc
jorge <jorgemartinezpizarro@gmail.com>
2025-03-23 17:39:33 +0100

    use placeholder name for helloworld nc app

--- nextcloud-mod-example/helloworld/templates/main.php
+++ nextcloud-mod-example/helloworld/templates/main.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MPTree Network</title>
+    <title>Hello World</title>
     <?php
         script('helloworld', 'index'); // Esto carga tu JS
     ?>
 
 </head>
 <body>
-    <button id="installApp" style="display: none;">📲 Instalar MPTree</button>
+    <button id="installApp" style="display: none;">📲 Instalar the HellowWorld Nextcloud Web App</button>
     <iframe src="URL OF YOUR APP" width="100%" height="100%" style="border:none;"></iframe>
 </body>
 </html>
