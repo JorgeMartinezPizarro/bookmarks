66f96926c8d1641da8b7f9879893b7b6adddf082
jorge <jorgemartinezpizarro@gmail.com>
2025-05-19 14:50:30 +0200

    fix oauth2 for 31.0.5 nextcloud

--- app/lib/oauth2.ts
+++ app/lib/oauth2.ts
@@ -1,5 +1,11 @@
-// lib/oauth2.ts
 import { AuthorizationCode } from 'simple-oauth2';
+
+if (!process.env.NEXTCLOUD_CLIENT_ID || !process.env.NEXTCLOUD_CLIENT_SECRET) {
+  throw new Error('Faltan variables de entorno para Nextcloud OAuth2');
+}
+
+// Puedes poner directamente el dominio sin depender de otra variable
+const tokenHost = process.env.NEXTCLOUD_URL!;
 
 const config = {
   client: {
@@ -7,7 +13,7 @@ const config = {
     secret: process.env.NEXTCLOUD_CLIENT_SECRET!,
   },
   auth: {
-    tokenHost: process.env.NEXTCLOUD_URL!,
+    tokenHost,
     tokenPath: '/apps/oauth2/api/v1/token',
     authorizePath: '/apps/oauth2/authorize',
   },
