2182d2636447bd79deb51d5c0760898aab004a9c
jorge <jorgemartinezpizarro@gmail.com>
2026-02-07 14:36:59 +0100

    update nextcloud module skeleton

--- nextcloud-mod-example/helloworld/templates/main.php
+++ nextcloud-mod-example/helloworld/templates/main.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
     <?php
-        script('helloworld', 'index'); // Esto carga tu JS
+        script('helloworld', 'index');
     ?>
 
 </head>
